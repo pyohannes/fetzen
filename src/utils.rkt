b239f117c626770ccf517463d3b03e93fa5bc3bb
Johannes Tax <johannes.tax@gmx.at>
2016-10-06 20:45:57 +0200

    Fixes #15: Document utils.rkt.

--- src/utils.rkt
+++ src/utils.rkt
@@ -1,11 +1,22 @@
-;;\section{\textit{utils.rkt}}
+;;```((mode docu) (post ("replace-variables")))
+;;\section{\textit{``(file-basename)``}}
+;;
+;;This module contains utility functions that are used by other parts of the
+;;program, but are not provided by racket.
+;;
+;;The \textit{racket/base} library is used:
 ;;```
 #lang racket/base
-
+;;```
+;;The function \textit{startswith?} returns \textit{\#t} if the string 
+;;\textit{line} starts with the string \textit{substr} and \textit{\#f} 
+;;otherwise:
+;;```
 (define (startswith? line substr)
-  (let ((len (string-length substr)))
-    (and (>= (string-length line) len)
-         (equal? (substring line 0 len) substr))))
-
-
-(provide startswith?)
+   (equal? substr
+           (substring line 0 (min (string-length line)
+                                  (string-length substr)))))
+;;```
+;;All defined functions are exported:
+;;```
+(provide (all-defined-out))
